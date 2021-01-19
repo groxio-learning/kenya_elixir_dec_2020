0d518add4fbacd2e01ce7c5e4ac4903e2d712831
Midigo Frank <midigofrank@gmail.com>
2021-01-19 19:45:33 +0300

    check game status

--- board.ex
+++ board.ex
@@ -5,8 +5,49 @@ defmodule MindGame.Board do
     %__MODULE__{answer: answer, guesses: []}
   end
 
-  def check(board, guess) do
+  def guess(board, guess) do
     %{ board | guesses: [guess|board.guesses]}
   end
+
+  def check(board, max_rows) do
+    %{rows: Enum.map(board.guesses, fn guess -> %{guess: guess, score: score(guess, board.answer)} end)}
+    |> put_status(max_rows)
+  
+  end
+
+  defp put_status(%{rows: [row | _]} = result, max_rows) do
+   status =  cond do
+     Enum.count(result.rows) < max_rows and has_white_or_none_keys?(row.score) ->
+        :playing
+
+      Enum.count(result.rows) <= max_rows and !has_white_or_none_keys?(row.score) ->
+        :won 
+
+      true ->
+      :lost
+
+     end
+      Map.merge(result, %{status: status})
+  end
+
+  defp has_white_or_none_keys?(score) do
+    Map.has_key?(score, :white) or Map.has_key?(score, :none)
+  end
+
+
+
+defp score(guess, ans) do
+ wrong_answers =  guess -- ans
+ Enum.zip(ans, guess) |> Enum.frequencies_by(fn {x, y} ->  
+ cond do
+   x == y -> :red
+   y in wrong_answers -> :none
+  true -> :white
+     
+ end
+ 
+ end)
+end
+
 
 end
